43bfd5a6b32506df8432f6b124a0ec6698ac8afd
DUVOISIN Nicolas <d16005292@a-maq-iutinfo.salsa.univ-amu.fr>
2018-12-17 17:11:19 +0100

    Ajout du début de participant (à finir)

--- src/fr/univamu/iut/traitement/acteur/Participant.java
+++ src/fr/univamu/iut/traitement/acteur/Participant.java
@@ -1,40 +1,45 @@
 package fr.univamu.iut.traitement.acteur;
 
+import fr.univamu.iut.traitement.Offre;
+import fr.univamu.iut.traitement.produit.ProduitFermier;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * 
+ *  Interface correspondant à l'ensemble des acteurs du systeme
  */
 public abstract class Participant {
 
     /**
-     * Default constructor
-     */
-    public Participant() {
-    }
-
-    /**
-     * 
+     * Solde disponible de l'acteur
      */
     private double solde;
 
     /**
-     * 
+     * Gains générés par l'acteur
      */
     private double gain;
 
-
+    /**
+     * Tableau contenant l'inventaire de l'acteur ( ses objets à vendre )
+     */
+    private List<ProduitFermier> inventaire = new ArrayList<>();
 
     /**
-     * 
+     * Fonction permettant de créer une offre
      */
-    public void creerOffreVente() {
-        // TODO implement here
+    public void creerOffreVente(int prix,ProduitFermier produitFermier) {
+        Offre offre = new Offre(this,produitFermier,prix);
     }
 
     /**
-     * @param Offre offre
+     * Permet de postuler à une offre pour être sélectionner pour l'achat
+     * @param offre : Offre dans laquelle on veut postuler
      */
-    public void creerOffreAchat(void Offre offre) {
+    public void creerOffreAchat(Offre offre) {
         // TODO implement here
     }
+
 
 }
