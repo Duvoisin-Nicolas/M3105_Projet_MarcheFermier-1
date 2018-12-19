5548636427af38a46f38efb5c53ac361eb1d3bb4
adrien-mollet <adrien.mollet@etu.univ-amu.fr>
2018-12-19 17:11:45 +0100

    Production de Pomme

--- src/fr/univamu/iut/traitement/production/ProductionPomme.java
+++ src/fr/univamu/iut/traitement/production/ProductionPomme.java
@@ -1,20 +1,25 @@
 package fr.univamu.iut.traitement.production;
 
+import fr.univamu.iut.traitement.produit.Pomme;
 import fr.univamu.iut.traitement.produit.ProduitFermier;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 /**
  * 
  */
-public class ProductionPomme implements UnitéDeProduction {
+public class ProductionPomme extends UniteDeProduction {
 
     /**
      * Default constructor
      */
-    public ProductionPomme() {
+    public ProductionPomme(int capaciteDeProduction) {
+        super(capaciteDeProduction);
     }
 
     @Override
     public ProduitFermier fabriquer() {
-        return null;
+        return new Pomme(LocalDate.now().plus(30, ChronoUnit.DAYS),getCapaciteDeProduction());
     }
 }
