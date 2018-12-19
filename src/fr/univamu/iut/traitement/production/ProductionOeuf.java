a46bfefbb69e167916d056c7ea9ebc8faa11d167
adrien-mollet <adrien.mollet@etu.univ-amu.fr>
2018-12-19 17:11:25 +0100

    Production d'Oeuf

--- src/fr/univamu/iut/traitement/production/ProductionOeuf.java
+++ src/fr/univamu/iut/traitement/production/ProductionOeuf.java
@@ -1,20 +1,25 @@
 package fr.univamu.iut.traitement.production;
 
+import fr.univamu.iut.traitement.produit.Oeuf;
 import fr.univamu.iut.traitement.produit.ProduitFermier;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 /**
  * 
  */
-public class ProductionOeuf implements UnitéDeProduction {
+public class ProductionOeuf extends UniteDeProduction {
 
     /**
      * Default constructor
      */
-    public ProductionOeuf() {
+    public ProductionOeuf(int capaciteDeProduction) {
+        super(capaciteDeProduction);
     }
 
     @Override
     public ProduitFermier fabriquer() {
-        return null;
+        return new Oeuf(LocalDate.now().plus(30, ChronoUnit.DAYS),getCapaciteDeProduction());
     }
 }
