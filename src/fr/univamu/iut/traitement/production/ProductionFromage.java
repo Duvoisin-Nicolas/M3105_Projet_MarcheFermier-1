28aafe2d86f0a6915a8b04cfc66c8b1641198624
adrien-mollet <adrien.mollet@etu.univ-amu.fr>
2018-12-19 17:10:48 +0100

    Production de Fromage

--- src/fr/univamu/iut/traitement/production/ProductionFromage.java
+++ src/fr/univamu/iut/traitement/production/ProductionFromage.java
@@ -1,20 +1,22 @@
 package fr.univamu.iut.traitement.production;
 
+import fr.univamu.iut.traitement.produit.Fromage;
 import fr.univamu.iut.traitement.produit.ProduitFermier;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 /**
  * 
  */
-public class ProductionFromage implements UnitéDeProduction {
+public class ProductionFromage extends UniteDeProduction {
 
-    /**
-     * Default constructor
-     */
-    public ProductionFromage() {
+    public ProductionFromage(int capaciteDeProduction) {
+        super(capaciteDeProduction);
     }
 
     @Override
     public ProduitFermier fabriquer() {
-        return null;
+        return new Fromage(LocalDate.now().plus(30, ChronoUnit.DAYS),getCapaciteDeProduction());
     }
 }
