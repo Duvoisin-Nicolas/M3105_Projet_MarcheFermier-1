1aa41e1d6ca92445161177c04f21224427fabf27
adrien-mollet <adrien.mollet@etu.univ-amu.fr>
2018-12-27 17:57:04 +0100

    Setter pour l'unite de prodution de l'arboriculteur

--- src/fr/univamu/iut/traitement/acteur/producteur/Arboriculteur.java
+++ src/fr/univamu/iut/traitement/acteur/producteur/Arboriculteur.java
@@ -8,12 +8,16 @@ import fr.univamu.iut.traitement.production.ProductionPomme;
 public class Arboriculteur extends Producteur {
 
     private static int compteurId = 0;
-    private ProductionPomme productionPomme = new ProductionPomme();
+    private ProductionPomme productionPomme;
     /**
      * Default constructor
      */
     public Arboriculteur() {
         this.setId("Arboriculteur " + ++compteurId);
+    }
+
+    public void setProductionPomme(ProductionPomme productionPomme) {
+        this.productionPomme = productionPomme;
     }
 
     @Override
