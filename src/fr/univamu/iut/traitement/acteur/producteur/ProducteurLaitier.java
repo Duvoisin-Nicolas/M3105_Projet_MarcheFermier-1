03b3463f0cd7e7372e679b4673279e65effcb486
adrien-mollet <adrien.mollet@etu.univ-amu.fr>
2018-12-27 18:00:56 +0100

    Setter pour l'unite de prodution du producteur laitier

--- src/fr/univamu/iut/traitement/acteur/producteur/ProducteurLaitier.java
+++ src/fr/univamu/iut/traitement/acteur/producteur/ProducteurLaitier.java
@@ -8,12 +8,16 @@ import fr.univamu.iut.traitement.production.ProductionFromage;
 public class ProducteurLaitier extends Producteur {
 
     private static int compteurId = 0;
-    private ProductionFromage productionFromage = new ProductionFromage();
+    private ProductionFromage productionFromage;
     /**
      * Default constructor
      */
     public ProducteurLaitier() {
         this.setId("Producteur Laitier " + ++compteurId);
+    }
+
+    public void setProductionFromage(ProductionFromage productionFromage) {
+        this.productionFromage = productionFromage;
     }
 
     @Override
