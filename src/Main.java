ac42949fb199f8b5bb11ece6f932f4162f081fdf
Barthélémy Drabczuk <barthelemy.drabczuk@etu.univ-amu.fr>
2018-12-31 18:06:11 +0100

    création de fonctions pour les vues (réutilisables pour le GUI si nécessaire) et ajout de doc

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,61 @@
 import fr.univamu.iut.traitement.Controleur;
 import fr.univamu.iut.traitement.Marche;
+import fr.univamu.iut.traitement.Offre;
 import fr.univamu.iut.traitement.acteur.Grossiste;
 import fr.univamu.iut.traitement.acteur.Participant;
 import fr.univamu.iut.traitement.acteur.Trader;
 import fr.univamu.iut.traitement.acteur.producteur.*;
 import fr.univamu.iut.traitement.production.*;
+import fr.univamu.iut.traitement.produit.ProduitFermier;
 
 import java.util.ArrayList;
 
 public class Main {
+
+    /**
+     * fonction pour afficher tous les participants du Marche
+     * //todo faire en sorte que ce soit le Marche qui permette d'acceder à ses participants
+     * @param participants
+     */
+    public void VueDesParticipants (ArrayList<Participant> participants) {
+        for (Participant participant : participants) {
+            System.out.println(participant);
+        }
+    }//VueDesParticipants
+
+    /**
+     * fonction pour afficher toutes les offres du Marche
+     * @param marche
+     */
+    public void VueDuCatalogue (Marche marche) {
+        for (Offre offre : marche.getOffres()) {
+            System.out.println(offre);
+        }
+    }//VueDuCatalogue
+
+    /**
+     * fonction pour afficher les transaction en temps réel
+     * @param marche
+     */
+    public void VueDuJournalDesTransactions (Marche marche) {
+        //TODO issue #37
+    }
+
+    /**
+     * fonction pour afficher les cotations de chaque produit du Marche
+     * @param marche
+     */
+    public void VueDesCotations (Marche marche) {
+        ArrayList<ProduitFermier> produitsDuMarche = new ArrayList<>();
+        for (Offre offre : marche.getOffres()) {
+            for (ProduitFermier produit : produitsDuMarche) {
+                if (offre.getProduitEnVente().getClass() == produit.getClass()) {
+
+                }
+            }
+        }
+    }//VueDesCotisations
+
     public void main (String [] args) {
         Marche marche = new Marche();
         Controleur controleur = new Controleur();
@@ -32,12 +79,6 @@ public class Main {
         tousLesParticipantsDuMarche.add(new Grossiste());
         tousLesParticipantsDuMarche.add(new Trader());
 
-        for (Participant participant : tousLesParticipantsDuMarche) {
-            if (participant instanceof Producteur) {
-                ((Producteur) participant).produire();
-            }
-
-        }
 
     }//main
 }
