8619be23e90702d6590ab65221c711d34de55d5c
nicolas-trouin <nicolas.trouin@etu.univ-amu.fr>
2019-01-03 20:56:07 +0100

    Correction classe abstraite Observable

--- src/fr/univamu/iut/traitement/produit/Observable.java
+++ src/fr/univamu/iut/traitement/produit/Observable.java
@@ -2,20 +2,11 @@ package fr.univamu.iut.traitement.produit;
 
 import fr.univamu.iut.traitement.acteur.IObservateur;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public abstract class Observable {
 
-    protected Set<IObservateur> mesAbonnes = new HashSet<>();
+    public abstract void ajouterAbonne(IObservateur observateur);
 
-    public void ajouterAbonne(IObservateur observateur){
-        mesAbonnes.add(observateur);
-    }
-
-    public void retirerAbonne(IObservateur observateur){
-        mesAbonnes.remove(observateur);
-    }
+    public abstract void retirerAbonne(IObservateur observateur);
 
     public abstract void notifier();
 }
