a5daed7dfb739e7a79fdac743caa49533b2dc883
nicolas-trouin <nicolas.trouin@etu.univ-amu.fr>
2019-01-03 20:53:09 +0100

    Correction IObservateur

--- src/fr/univamu/iut/traitement/acteur/IObservateur.java
+++ src/fr/univamu/iut/traitement/acteur/IObservateur.java
@@ -1,7 +1,9 @@
 package fr.univamu.iut.traitement.acteur;
 
+import fr.univamu.iut.traitement.produit.ProduitFermier;
+
 public interface IObservateur {
 
-    void mettreAJour();
+    void mettreAJour(ProduitFermier produit);
 
 }
